d750a56c14248e76989cf1e70b75579273d9e0c0
hami-r <mynameissaitama54@gmail.com>
2022-12-05 15:30:45 +0530

    controller package and routed to homepage

---
+++ src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.nest.employeeapp_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String  homepage(){
+        return "Welcome to my website";
+    }
+}
