c68ea3c7605cb3b08d68e5a54ae8cf5187de66d7
hami-r <mynameissaitama54@gmail.com>
2022-12-06 10:15:13 +0530

    model class created

--- src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
@@ -1,13 +1,15 @@
 package com.nest.employeeapp_backend.controller;
 
+import com.nest.employeeapp_backend.model.EmployeeModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
-    @PostMapping("/")
+    @GetMapping("/")
     public String  homepage(){
         return "Welcome to my website";
     }
@@ -17,9 +19,10 @@ public class EmployeeController {
         return "Welcome to View page";
     }
 
-    @PostMapping("/add")
-    public String addpage(){
-        return "Welcome to Add page";
+    @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
+    public String addpage(@RequestBody EmployeeModel e){
+        System.out.println(e.getName());
+        return "Employee added successfully";
     }
 
     @PostMapping("/search")
---
+++ src/main/java/com/nest/employeeapp_backend/model/EmployeeModel.java
@@ -0,0 +1,100 @@
+package com.nest.employeeapp_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class EmployeeModel {
+
+    @GeneratedValue
+    @Id
+    private int id;
+    private String name;
+    private String designation;
+    private int salary;
+    private String companyName;
+    private String mobileNo;
+    private String username;
+    private String password;
+
+    public EmployeeModel() {
+    }
+
+    public EmployeeModel(int id, String name, String designation, int salary, String companyName, String mobileNo, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+        this.companyName = companyName;
+        this.mobileNo = mobileNo;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
